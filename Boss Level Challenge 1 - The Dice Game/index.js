01111bbd7eb62e38eb85f69f469c82d3064b3255
klin4744 <klin4744@bths.edu>
2019-01-10 14:51:23 -0500

    Created and finish first project

---
+++ Boss Level Challenge 1 - The Dice Game/index.js
@@ -0,0 +1,13 @@
+let dice1Roll = Math.floor(Math.random() * 6) + 1;
+let dice2Roll = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").src = `./images/dice${dice1Roll}.png`;
+document.querySelector(".img2").src = `./images/dice${dice2Roll}.png`;
+const text = document.querySelector("h1");
+
+if (dice1Roll === dice2Roll) {
+  text.innerText = "It's a draw!";
+} else {
+  dice1Roll > dice2Roll
+    ? (text.innerText = "Player 1 Wins!")
+    : (text.innerText = "Player 2 Wins!");
+}
