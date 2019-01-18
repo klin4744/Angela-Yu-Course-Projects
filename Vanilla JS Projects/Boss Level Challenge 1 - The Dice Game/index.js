8d7882f8d82de7ebeed9ced9696fce945eea58be
klin4744 <klin4744@bths.edu>
2019-01-17 22:20:52 -0500

    Update

--- Vanilla JS Projects/Boss Level Challenge 1 - The Dice Game/index.js
+++ Vanilla JS Projects/Boss Level Challenge 1 - The Dice Game/index.js
@@ -2,15 +2,19 @@ let dice1Roll, dice2Roll;
 document.querySelector("button").addEventListener("click", () => {
   dice1Roll = Math.floor(Math.random() * 6) + 1;
   dice2Roll = Math.floor(Math.random() * 6) + 1;
-  document.querySelector(".img1").src = `./images/dice${dice1Roll}.png`;
-  document.querySelector(".img2").src = `./images/dice${dice2Roll}.png`;
+  document
+    .querySelector(".img1")
+    .setAttribute("src", `./images/dice${dice1Roll}.png`);
+  document
+    .querySelector(".img2")
+    .setAttribute("src", `./images/dice${dice2Roll}.png`);
   const text = document.querySelector("h1");
 
   if (dice1Roll === dice2Roll) {
     text.innerText = "It's a draw!";
   } else {
     dice1Roll > dice2Roll
-      ? (text.innerText = "Player 1 Wins!")
-      : (text.innerText = "Player 2 Wins!");
+      ? (text.innerText = "🏁 Player 1 Wins!")
+      : (text.innerText = "Player 2 Wins! 🏁");
   }
 });
