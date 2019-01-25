22849baf1ac3a889143c17835d7d4950c6e7679d
klin4744 <klin4744@bths.edu>
2019-01-24 23:20:16 -0500

    Finish V1

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = ["Do Work"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = ["Do Work"];
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -11,13 +12,7 @@ app.set("view engine", "ejs");
 // we needs a views folder to hold ejs files, the engine will look into the views folder by default
 
 app.get("/", function(req, res) {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  day = date.getDate();
 
   // The key has to match what value you put in the EJS marker! This render method renders the list.ejs file in our views folder and passes the variable day as kindOfDay to our ejs file
   res.render("list", { listTitle: day, items });
